cbd7a55a85f3ffc88c690b37b63e63cad26afa16
William Schaub <william.schaub@genesi-usa.com>
2011-08-29 17:33:58 -0400

    Modified powerbutton to run as a real daemon by default and to use the standard getopt(3) library call to get commnad line arguments (mainly -f to make it run in the foreground) I also removed the old RCS log from the source file.

--- services/powerbutton/powerbutton.c
+++ services/powerbutton/powerbutton.c
@@ -1,5 +1,7 @@
 /* $Id: powerbutton.c,v 1.3 2011/06/23 07:35:36 wschaub Exp wschaub $  */
 /* read /dev/input/event0 forever until the power key is pressed and then initiate a clean poweroff */
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <linux/input.h>
@@ -11,10 +13,33 @@
 }; */
 
 int main(int argc, char **argv) {
+int opt,foreground; 
+foreground = 0;
+
+while ((opt = getopt(argc, argv, "f")) != -1) {
+    switch (opt) {
+           case 'f':
+               foreground = 1;
+               break;
+           default: /* '?' */
+               fprintf(stderr, "Usage: %s [ -f ]\n",
+                       argv[0]);
+               exit(EXIT_FAILURE);
+           }
+}
+
+if(!foreground) {
+    daemon(0,0);
+}
 
 struct input_event event; //Each read of event0 gives us an input_event struct
 FILE *fp;
 fp = fopen("/dev/input/event0","r"); //XXX! we should check for errors here...
+if(fp == NULL)
+{
+    perror("/dev/input/event0");
+    abort();
+}
 
 //Loop forever reading events and do something only when the power key is pressed.
     while(1) 
@@ -31,15 +56,4 @@ fp = fopen("/dev/input/event0","r"); //XXX! we should check for errors here...
 
 }
 
-/*
- * $Log: powerbutton.c,v $
- * Revision 1.3  2011/06/23 07:35:36  wschaub
- * we now execute /sbin/poweroff on a power button press
- *
- * Revision 1.2  2011/06/23 07:24:40  wschaub
- * changed exit to return
- *
- * Revision 1.1  2011/06/23 07:20:28  wschaub
- * Initial revision
- *
- */
+
